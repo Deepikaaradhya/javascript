8fc1018fecb65df0ab9a24e57aa0268b912af0a9
Deepikaaradhya <deepikaaradhya62@gmail.com>
2021-08-14 08:49:36 -0700

    javascript_code

---
+++ first.js
@@ -0,0 +1,127 @@
+var a = 10
+var b = 20
+a+b (addition)
+30
+
+var a = "Hii"
+var b = "javascript"
+a+b (concat)
+"Hijavascript"
+
+var a = 10
+var b = 20
+a-b
+-10
+b-a
+10
+a*b
+200
+a/b
+0.5
+
+var a = "Hi"
+var b = "javascript"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+a+b
+"Hijavascript"
+
+var a = 10
+var b = 20
+b%a
+0
+2%4
+2
+3%4
+3
+4%4
+0
+5%4
+1
+7%4
+3
+100%101
+100
+7%4
+3
+6%4
+2
+8%4
+0
+
+var a = "Hi"
+var b = 10
+a+b
+"Hi10"
+a-b
+NaN
+a*b
+NaN
+
+var a =true
+var b = "a10b11"
+a+b
+"truea10b11"
+a/b
+NaN
+a*b
+NaN
+b+a
+"a10b11true"
+b%a
+NaN
+b%b
+NaN
+a-b
+NaN
+b-a
+NaN
+
+"a10"+20+30
+"a102030"
+10+20.99+"30"
+"30.9930"
+"1a11"+30+"40"
+"1a113040"
+true+10+"10"
+"1110"
+true-"20"*5
+-99
+false*10-"2"
+-2
+29.88-"a101"+2
+NaN
+"20.3"+10-2
+18.31
+"false"+10-3
+NaN
+
+"10a"-5
+NaN
+"20"-a
+19
+10%"flase"
+NaN
+"false"+10
+"false10"
+"false"+40+"false"
+"false40false"
+"false"+"true"
+"falsetrue"
+"true"+20-false
+NaN
+
+false*false
+0
+false*"true"
+NaN
+true*28.9090
+28.909
+"false"*40.5
+NaN
+20.33%"10"
+0.3299999999999983
---
+++ second.js
@@ -0,0 +1,122 @@
+var a= true
+var b= false
+var c= true
+a+c
+2
+a+b
+1
+true+true
+2
+
+var a = "true"
+var b = false
+var c = 20
+a+c
+"true20"
+b+c
+20
+b-a
+NaN
+b*c
+0
+c/b
+Infinity
+a/b
+NaN
+b/a
+NaN
+c*a+b
+NaN
+a+b-c
+NaN
+c/b+a
+"Infinitytrue"
+
+var a = 10
+var b = "hi"
+
+typeof(a)
+"number"
+typeof(b)
+"string"
+
+var c = true
+
+typeof(c)
+"boolean"
+
+var d = "false"
+undefined
+var e ="20.99"
+undefined
+var f = 30.1212
+undefined
+var g='70'
+undefined
+
+"10"+5
+"105"
+
+var a ="10"
+undefined
+var b = "20"
+undefined
+a+b
+"1020"
+parseInt(a)+parseInt(b)
+30
+parseInt(a)-parseInt(b)
+-10
+var a = "101abc11"
+undefined
+parseInt(a)
+101
+parseFloat(a)
+101
+
+var a = "10.11"
+undefined
+Number(a)
+10.11
+var a = '10a11'
+undefined
+Number(a)
+NaN
+prompt("what is ur name")
+"deepika"
+var a = prompt("enter first number")
+undefined
+var b = ("enter second number")
+undefined
+30
+30
+var a=prompt("Enter first Number")
+undefined
+var b = prompt("Enter second Number")
+undefined
+a+b
+"2020"
+a-b
+0
+a*b
+400
+
+var a=prompt("Enter ur Name")
+undefined
+var b = prompt("Enter ur last number")
+undefined
+
+a+b
+"deepikaKM"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+a%b
+NaN
+b%a
+NaN
+b+a
+"KMdeepika"
