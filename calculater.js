d98ab9c80ce4f50ed2f66e3f6f646d92ae6bc8b6
Deepikaaradhya <deepikaaradhya62@gmail.com>
2021-08-16 10:29:28 -0700

    calc

---
+++ calculater.js
@@ -0,0 +1,34 @@
+function add(){
+    var a = document.getElementById("first").value;
+    var b = document.getElementById("second").value;
+    var out = Number(a)+Number(b)
+    document.getElementsByClassName("output")[0].innerText="Addition is "+out
+}
+
+function sub(){
+    var a = document.getElementById("first").value;
+    var b = document.getElementById("second").value;
+    var out = Number(a)-Number(b)
+    document.getElementsByClassName("output")[0].innerText="Subtraction is "+out
+}
+
+function multi(){
+    var a = document.getElementById("first").value;
+    var b = document.getElementById("second").value;
+    var out = Number(a)*Number(b)
+    document.getElementsByClassName("output")[0].innerText="Multiplication is "+out
+}
+
+function div(){
+    var a = document.getElementById("first").value;
+    var b = document.getElementById("second").value;
+    var out = Number(a)/Number(b)
+    document.getElementsByClassName("output")[0].innerText="Division is "+out
+}
+
+function per(){
+    var a = document.getElementById("first").value;
+    var b = document.getElementById("second").value;
+    var out = Number(a)%Number(b)
+    document.getElementsByClassName("output")[0].innerText="Percentage is "+out
+}
